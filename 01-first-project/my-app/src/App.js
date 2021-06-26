1ae7f4aa392a6cd484576742f939e7ab38dc4a0d
Dmytro Kondaurov <invisibl@ukr.net>
2021-06-26 14:13:05 +0300

    add public & src

---
+++ 01-first-project/my-app/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      Simpl HTML
+      <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JS</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
