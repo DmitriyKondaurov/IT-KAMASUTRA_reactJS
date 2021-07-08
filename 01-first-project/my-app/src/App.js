94fdfccd051d88b15c63fd1a86678a703b7bf945
Dmytro Kondaurov <invisibl@ukr.net>
2021-07-08 15:08:31 +0300

    lesson #7

--- 01-first-project/my-app/src/App.js
+++ 01-first-project/my-app/src/App.js
@@ -1,17 +1,34 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import React, {Component} from "react";
 import './App.css';
 
 const App = () => {
   return (
-    <div className="App">
-      Simpl HTML
-      <ul>
-        <li>HTML</li>
-        <li>CSS</li>
-        <li>JS</li>
-      </ul>
-    </div>
+      <div>
+          <Header />
+          <Technologies />
+      </div>
   );
-}
+};
+
+const Technologies = () => {
+    return (
+    <div>
+        This is Technologies
+        <ul>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>JS</li>
+        </ul>
+    </div>);
+};
+
+const Header = () => {
+    return (
+        <div className="Header">
+            <div>This is Header</div>
+        </div>
+    );
+};
 
 export default App;
