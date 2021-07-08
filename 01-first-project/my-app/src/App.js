abe601367434b7d719a67c815446c994374ea135
Dmytro Kondaurov <invisibl@ukr.net>
2021-07-09 00:47:23 +0300

    lesson #11

--- 01-first-project/my-app/src/App.js
+++ 01-first-project/my-app/src/App.js
@@ -1,15 +1,41 @@
 // import logo from './logo.svg';
 // import React, {Component} from "react";
-import Header from "./Header";
-import Technologies from "./Technologies";
 import React from "react";
 import './App.css';
 
 const App = () => {
     return (
-        <div>
-            <Header/>
-            <Technologies/>
+        <div className='app-wrapper'>
+            <header className='header'>
+                <img src="https://img.freepik.com/free-vector/chain-logo-design_1465-159.jpg?size=626&ext=jpg" alt=""/>
+            </header>
+            <nav className='nav'>
+                <ul>
+                    <li><a href="">Profile</a></li>
+                    <li><a href="">Messages</a></li>
+                    <li><a href="">News</a></li>
+                    <li><a href="">Music</a></li>
+                    <li><a href="">Settings</a></li>
+                </ul>
+            </nav>
+            <main className='content'>
+                <div>
+                    <img src="https://www.imgacademy.com/themes/custom/imgacademy/images/helpbox-contact.jpg" alt=""/>
+                </div>
+                <div>ava + discription</div>
+                <div>
+                    My posts
+                    <div>New post</div>
+                    <div>
+                        <div>Post 1</div>
+                        <div>Post 2</div>
+                    </div>
+                </div>
+                <div></div>
+            </main>
+            <footer className='footer'>
+                This is footer
+            </footer>
         </div>
     );
 };
