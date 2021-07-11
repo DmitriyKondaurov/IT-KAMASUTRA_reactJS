0fbe490c3cdc2f2ec40bd368db7d86eea78a7161
Dmytro Kondaurov <invisibl@ukr.net>
2021-07-11 19:50:21 +0300

    lesson #12

--- 01-first-project/my-app/src/App.js
+++ 01-first-project/my-app/src/App.js
@@ -2,42 +2,19 @@
 // import React, {Component} from "react";
 import React from "react";
 import './App.css';
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+import Profile from "./components/Profile";
+import Footer from "./components/Footer";
 
 const App = () => {
     return (
         <div className='app-wrapper'>
-            <header className='header'>
-                <img src="https://img.freepik.com/free-vector/chain-logo-design_1465-159.jpg?size=626&ext=jpg" alt=""/>
-            </header>
-            <nav className='nav'>
-                <ul>
-                    <li><a href="">Profile</a></li>
-                    <li><a href="">Messages</a></li>
-                    <li><a href="">News</a></li>
-                    <li><a href="">Music</a></li>
-                    <li><a href="">Settings</a></li>
-                </ul>
-            </nav>
-            <main className='content'>
-                <div>
-                    <img src="https://www.imgacademy.com/themes/custom/imgacademy/images/helpbox-contact.jpg" alt=""/>
-                </div>
-                <div>ava + discription</div>
-                <div>
-                    My posts
-                    <div>New post</div>
-                    <div>
-                        <div>Post 1</div>
-                        <div>Post 2</div>
-                    </div>
-                </div>
-                <div></div>
-            </main>
-            <footer className='footer'>
-                This is footer
-            </footer>
-        </div>
-    );
+            <Header />
+            <Navbar />
+            <Profile />
+            <Footer />
+        </div>);
 };
 
 export default App;
---
+++ 01-first-project/my-app/src/components/Profile.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <main className='content'>
+            <div>
+                <img src="https://www.imgacademy.com/themes/custom/imgacademy/images/helpbox-contact.jpg" alt=""/>
+            </div>
+            <div>ava + discription</div>
+            <div>
+                My posts
+                <div>New post</div>
+                <div>
+                    <div>Post 1</div>
+                    <div>Post 2</div>
+                </div>
+            </div>
+        </main>
+    );
+};
+
+export default Profile;
