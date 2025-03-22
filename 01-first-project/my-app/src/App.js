ed93e4748f92e4a3ca8419ac402c43d5ff064645
invisible <invisibilityclear@gmail.com>
2025-03-22 18:25:16 +0100

    lesson 11 footer

--- 01-first-project/my-app/src/App.js
+++ 01-first-project/my-app/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Technologies from "./components/Technologies/technologies";
 import Header from "./components/Header/header";
+import Footer from "./components/Footer/footer";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Technologies />
+      <Footer />
     </div>
   );
 }
---
+++ 01-first-project/my-app/src/components/Footer/footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <footer className="footer">
+            <div>Copyrights</div>
+        </footer>
+    )
+}
+export default Footer;
