8ef64eda4707d894b98bbdf31e5682a0cd651862
DmitriyKondaurov <invisibl@ukr.net>
2025-04-05 18:27:38 +0200

    lesson #11 done

--- 01-first-project/my-app/src/App.js
+++ 01-first-project/my-app/src/App.js
@@ -27,10 +27,13 @@ function App() {
         <div className="app-content">
             <img src='https://images.pexels.com/photos/1563356/pexels-photo-1563356.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='image'/>
             <div>ava + description</div>
-            <div>my posts</div>
-            <div>New post</div>
-            <div>post 1</div>
-            <div>post 2</div>
+            <div>my posts
+                <div>New post</div>
+                <div>
+                    <div>post 1</div>
+                    <div>post 2</div>
+                </div>
+            </div>
         </div>
     </div>
   );
